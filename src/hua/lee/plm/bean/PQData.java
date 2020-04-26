d78c92e288b39672a4dea53141fd97e194277702
lijie <lijie@formovie.cn>
2020-04-26 19:35:53 +0800

    增加 PQ 数据解析

---
+++ src/hua/lee/plm/bean/PQData.java
@@ -0,0 +1,245 @@
+package hua.lee.plm.bean;
+
+public class PQData {
+    private String SN;
+    private String NormalGainRed;
+    private String NormalGainGreen;
+    private String NormalGainBlue;
+    private String CoolGainRed;
+    private String CoolGainGreen;
+    private String CoolGainBlue;
+    private String WarmGainRed;
+    private String WarmGainGreen;
+    private String WarmGainBlue;
+
+    private String NormalOffRed;
+    private String NormalOffGreen;
+    private String NormalOffBlue;
+    private String CoolOffRed;
+    private String CoolOffGreen;
+    private String CoolOffBlue;
+    private String WarmOffRed;
+    private String WarmOffGreen;
+    private String WarmOffBlue;
+
+    public String getSN() {
+        return SN;
+    }
+
+    public void setSN(String sn) {
+        if (sn.contains("/")) sn = sn.replace("/", "");
+        this.SN = sn;
+    }
+
+    public String getNormalGainRed() {
+        return NormalGainRed;
+    }
+
+    public void setNormalGainRed(String normalGainRed) {
+        NormalGainRed = normalGainRed;
+    }
+
+    public String getNormalGainGreen() {
+        return NormalGainGreen;
+    }
+
+    public void setNormalGainGreen(String normalGainGreen) {
+        NormalGainGreen = normalGainGreen;
+    }
+
+    public String getNormalGainBlue() {
+        return NormalGainBlue;
+    }
+
+    public void setNormalGainBlue(String normalGainBlue) {
+        NormalGainBlue = normalGainBlue;
+    }
+
+    public String getCoolGainRed() {
+        return CoolGainRed;
+    }
+
+    public void setCoolGainRed(String coolGainRed) {
+        CoolGainRed = coolGainRed;
+    }
+
+    public String getCoolGainGreen() {
+        return CoolGainGreen;
+    }
+
+    public void setCoolGainGreen(String coolGainGreen) {
+        CoolGainGreen = coolGainGreen;
+    }
+
+    public String getCoolGainBlue() {
+        return CoolGainBlue;
+    }
+
+    public void setCoolGainBlue(String coolGainBlue) {
+        CoolGainBlue = coolGainBlue;
+    }
+
+    public String getWarmGainRed() {
+        return WarmGainRed;
+    }
+
+    public void setWarmGainRed(String warmGainRed) {
+        WarmGainRed = warmGainRed;
+    }
+
+    public String getWarmGainGreen() {
+        return WarmGainGreen;
+    }
+
+    public void setWarmGainGreen(String warmGainGreen) {
+        WarmGainGreen = warmGainGreen;
+    }
+
+    public String getWarmGainBlue() {
+        return WarmGainBlue;
+    }
+
+    public void setWarmGainBlue(String warmGainBlue) {
+        WarmGainBlue = warmGainBlue;
+    }
+
+    public String getNormalOffRed() {
+        return NormalOffRed;
+    }
+
+    public void setNormalOffRed(String normalOffRed) {
+        NormalOffRed = normalOffRed;
+    }
+
+    public String getNormalOffGreen() {
+        return NormalOffGreen;
+    }
+
+    public void setNormalOffGreen(String normalOffGreen) {
+        NormalOffGreen = normalOffGreen;
+    }
+
+    public String getNormalOffBlue() {
+        return NormalOffBlue;
+    }
+
+    public void setNormalOffBlue(String normalOffBlue) {
+        NormalOffBlue = normalOffBlue;
+    }
+
+    public String getCoolOffRed() {
+        return CoolOffRed;
+    }
+
+    public void setCoolOffRed(String coolOffRed) {
+        CoolOffRed = coolOffRed;
+    }
+
+    public String getCoolOffGreen() {
+        return CoolOffGreen;
+    }
+
+    public void setCoolOffGreen(String coolOffGreen) {
+        CoolOffGreen = coolOffGreen;
+    }
+
+    public String getCoolOffBlue() {
+        return CoolOffBlue;
+    }
+
+    public void setCoolOffBlue(String coolOffBlue) {
+        CoolOffBlue = coolOffBlue;
+    }
+
+    public String getWarmOffRed() {
+        return WarmOffRed;
+    }
+
+    public void setWarmOffRed(String warmOffRed) {
+        WarmOffRed = warmOffRed;
+    }
+
+    public String getWarmOffGreen() {
+        return WarmOffGreen;
+    }
+
+    public void setWarmOffGreen(String warmOffGreen) {
+        WarmOffGreen = warmOffGreen;
+    }
+
+    public String getWarmOffBlue() {
+        return WarmOffBlue;
+    }
+
+    public void setWarmOffBlue(String warmOffBlue) {
+        WarmOffBlue = warmOffBlue;
+    }
+
+    @Override
+    public String toString() {
+        return "PQData{" +
+                "SN='" + SN + '\'' +
+                ", NormalGainRed='" + NormalGainRed + '\'' +
+                ", NormalGainGreen='" + NormalGainGreen + '\'' +
+                ", NormalGainBlue='" + NormalGainBlue + '\'' +
+                ", CoolGainRed='" + CoolGainRed + '\'' +
+                ", CoolGainGreen='" + CoolGainGreen + '\'' +
+                ", CoolGainBlue='" + CoolGainBlue + '\'' +
+                ", WarmGainRed='" + WarmGainRed + '\'' +
+                ", WarmGainGreen='" + WarmGainGreen + '\'' +
+                ", WarmGainBlue='" + WarmGainBlue + '\'' +
+                ", NormalOffRed='" + NormalOffRed + '\'' +
+                ", NormalOffGreen='" + NormalOffGreen + '\'' +
+                ", NormalOffBlue='" + NormalOffBlue + '\'' +
+                ", CoolOffRed='" + CoolOffRed + '\'' +
+                ", CoolOffGreen='" + CoolOffGreen + '\'' +
+                ", CoolOffBlue='" + CoolOffBlue + '\'' +
+                ", WarmOffRed='" + WarmOffRed + '\'' +
+                ", WarmOffGreen='" + WarmOffGreen + '\'' +
+                ", WarmOffBlue='" + WarmOffBlue + '\'' +
+                '}';
+    }
+
+//    public String toCSV() {
+//        return SN + "," +
+//                CoolGainRed + "," + CoolGainGreen + "," + CoolGainBlue + "," +
+//                CoolOffRed + "," + CoolOffGreen + "," + CoolOffBlue + "," +
+//                NormalGainRed + "," + NormalGainGreen + "," + NormalGainBlue + "," +
+//                NormalOffRed + "," + NormalOffGreen + "," + NormalOffBlue + "," +
+//                WarmGainRed + "," + WarmGainGreen + "," + WarmGainBlue + "," +
+//                WarmOffRed + "," + WarmOffGreen + "," + WarmOffBlue + "\n";
+//    }
+
+    public String toCSVNew() {
+        return SN + "," +
+                NormalGainRed + "," + CoolGainRed + "," + WarmGainRed + "," +
+                NormalGainGreen + "," + CoolGainGreen + "," + WarmGainGreen + "," +
+                NormalGainBlue + "," + CoolGainBlue + "," + WarmGainBlue + "," +
+                WarmOffRed + "," + WarmOffGreen + "," + WarmOffBlue + "," +
+                CoolOffRed + "," + CoolOffGreen + "," + CoolOffBlue + "," +
+                NormalOffRed + "," + NormalOffGreen + "," + NormalOffBlue + "\n";
+    }
+
+    public boolean containNull() {
+        return (CoolGainRed == null ||
+                CoolGainGreen == null ||
+                CoolGainBlue == null ||
+                CoolOffRed == null ||
+                CoolOffGreen == null ||
+                CoolOffBlue == null ||
+                NormalGainRed == null ||
+                NormalGainGreen == null ||
+                NormalGainBlue == null ||
+                NormalOffRed == null ||
+                NormalOffGreen == null ||
+                NormalOffBlue == null ||
+                WarmGainRed == null ||
+                WarmGainGreen == null ||
+                WarmGainBlue == null ||
+                WarmOffRed == null ||
+                WarmOffGreen == null ||
+                WarmOffBlue == null ||
+                SN == null || !SN.matches("[0-9]{13}")
+        );
+    }
+}
---
+++ src/hua/lee/plm/fm/PQParse.java
@@ -0,0 +1,187 @@
+package hua.lee.plm.fm;
+
+import hua.lee.plm.bean.PQData;
+
+import java.io.*;
+import java.util.*;
+
+public class PQParse {
+    public static void main(String[] args) throws IOException {
+        String[] PIDs = new String[]{
+                "18301",
+                "19301",
+                "19302",
+                "21236",
+                "20301",
+                "20302",
+                "20303",
+                "28022",
+                "27859",
+                "27858",
+        };
+        List<String> dataPaths = new ArrayList<>();
+        for (int i = 1; i < 13; i++) {
+            dataPaths.add("/Users/lijie/Desktop/testlog/testlog" + i + ".txt");
+        }
+
+        for (String pid : PIDs) {
+            for (String dataPath : dataPaths) {
+                generatePQData(pid, dataPath);
+            }
+        }
+
+    }
+
+
+    private static void pqDataParse(PQData pqData, String data) {
+        String value = parseData(data);
+        if (data.contains("读取标准色温白平衡红色增益")) {
+            pqData.setNormalGainRed(value);
+        }
+        if (data.contains("读取冷色色温白平衡红色增益")) {
+            pqData.setCoolGainRed(value);
+        }
+        if (data.contains("读取暖色色温白平衡红色增益")) {
+            pqData.setWarmGainRed(value);
+        }
+        if (data.contains("读取标准色温白平衡绿色增益")) {
+            pqData.setNormalGainGreen(value);
+        }
+        if (data.contains("读取冷色色温白平衡绿色增益")) {
+            pqData.setCoolGainGreen(value);
+        }
+        if (data.contains("读取暖色色温白平衡绿色增益")) {
+            pqData.setWarmGainGreen(value);
+        }
+        if (data.contains("读取标准色温白平衡蓝色增益")) {
+            pqData.setNormalGainBlue(value);
+        }
+        if (data.contains("读取冷色色温白平衡蓝色增益")) {
+            pqData.setCoolGainBlue(value);
+        }
+        if (data.contains("读取暖色色温白平衡蓝色增益")) {
+            pqData.setWarmGainBlue(value);
+        }
+        if (data.contains("读取标准色温白平衡红色偏移")) {
+            pqData.setNormalOffRed(value);
+        }
+        if (data.contains("读取冷色色温白平衡红色偏移")) {
+            pqData.setCoolOffRed(value);
+        }
+        if (data.contains("读取暖色色温白平衡红色偏移")) {
+            pqData.setWarmOffRed(value);
+        }
+        if (data.contains("读取标准色温白平衡绿色偏移")) {
+            pqData.setNormalOffGreen(value);
+        }
+        if (data.contains("读取冷色色温白平衡绿色偏移")) {
+            pqData.setCoolOffGreen(value);
+        }
+        if (data.contains("读取暖色色温白平衡绿色偏移")) {
+            pqData.setWarmOffGreen(value);
+        }
+        if (data.contains("读取标准色温白平衡蓝色偏移")) {
+            pqData.setNormalOffBlue(value);
+        }
+        if (data.contains("读取冷色色温白平衡蓝色偏移")) {
+            pqData.setCoolOffBlue(value);
+        }
+        if (data.contains("读取暖色色温白平衡蓝色偏移")) {
+            pqData.setWarmOffBlue(value);
+        }
+    }
+
+    private static String parseData(String data) {
+        if (data.length() < 75) {
+            return null;
+        }
+        String res = data.substring(69, 73);
+        if (res.contains("\"")) res = res.replace("\"", "");
+        System.out.println("parse data " + res);
+        return res;
+    }
+
+    private static void generatePQData(String PID, String dataPath) throws IOException {
+        List<String> snList = new ArrayList<>();
+        File file = new File("/Users/lijie/Desktop/pq/PID-" + PID + "-" + new Date() + ".csv");
+        //File file = new File("/Users/lijie/Desktop/pq/FM05501.csv");
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        //写入 CSV 标题
+        writer.append("SN,N_RGain,C_RGain,W_RGain,N_GGain,C_GGain,W_GGain,N_BGain,C_BGain,W_BGain,W_ROffset,W_GOffset,W_BOffset,C_ROffset,C_GOffset,C_BOffset,N_ROffset,N_GOffset,N_BOffset\n");
+
+
+        String data;
+        String oldSN = null;
+        PQData pqData = null;
+        Set<String> invalidCount = new HashSet<>();
+
+        //第一遍按顺序读取数据，记录异常数据 SN
+        //异常原因是因为 SN 数据不连贯
+        BufferedReader reader = new BufferedReader(new FileReader(dataPath));
+        while ((data = reader.readLine()) != null) {
+            String sn = data.substring(12, 26);
+            if (oldSN == null || !oldSN.equals(sn)) {
+                if (pqData != null && !snList.contains(pqData.getSN())) {
+                    if (pqData.containNull()) {
+                        invalidCount.add(pqData.getSN());
+                    } else {
+                        //判断是否为需要提取的产品数据 PID
+                        if (pqData.getSN().startsWith(PID)) {
+                            writer.append(pqData.toCSVNew());
+                            snList.add(pqData.getSN());
+                        }
+                    }
+
+                }
+                System.out.println("new SN,we create new File");
+                pqData = new PQData();
+                pqData.setSN(sn);
+            } else {
+                System.out.println("old sn " + sn);
+            }
+            pqDataParse(pqData, data);
+            oldSN = sn;
+            System.out.println(pqData.toString());
+        }
+        reader.close();
+
+        System.out.println("invalid sn list = " + Arrays.toString(invalidCount.toArray()));
+        System.out.println("invalid sn count = " + invalidCount.size());
+
+        Map<String, PQData> fixedMap = new HashMap<>();
+        for (String sn : invalidCount) {
+            if (sn.matches("[0-9]{13}")) {
+                PQData temp = new PQData();
+                temp.setSN(sn);
+                fixedMap.put(sn, temp);
+            }
+        }
+        //第二遍重新查询异常 SN 的数据
+        reader = new BufferedReader(new FileReader(dataPath));
+        while ((data = reader.readLine()) != null) {
+            for (String sn : invalidCount) {
+                if (data.contains(sn.substring(5))) {
+                    System.out.println("find fixed Data");
+                    PQData temp = fixedMap.get(sn);
+                    if (temp != null) {
+                        pqDataParse(temp, data);
+                    }
+                    break;
+                }
+            }
+        }
+
+        for (String sn : invalidCount) {
+            if (sn.startsWith(PID)) {
+                writer.append(fixedMap.get(sn).toCSVNew());
+                System.out.println("fixed sn PQ data = " + fixedMap.get(sn).toCSVNew());
+            }
+        }
+
+        reader.close();
+        writer.flush();
+    }
+}
