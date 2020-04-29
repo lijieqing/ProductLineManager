0e0829670ecf4a5150842b5c0844998980350896
lijie <lijie@formovie.cn>
2020-04-29 18:29:54 +0800

    优化 PQ 解析,修复多线程 bug

--- src/hua/lee/plm/fm/PQParse.java
+++ src/hua/lee/plm/fm/PQParse.java
@@ -17,6 +17,7 @@ public class PQParse {
     private static CountDownLatch latch;
 
     public static void main(String[] args) throws Exception {
+        System.out.println(new Date());
         mapPidPath = new HashMap<>();
         //step 1
         ParseLog();
@@ -24,18 +25,9 @@ public class PQParse {
         //step 2
         FinalCSV();
 
+        System.out.println(new Date());
     }
 
-    private static void ClearMiddle() {
-        for (List<String> values : mapPidPath.values()) {
-            for (String value : values) {
-                File file = new File(value);
-                if (file.exists()){
-                    file.delete();
-                }
-            }
-        }
-    }
 
     /**
      * STEP 1
@@ -70,17 +62,106 @@ public class PQParse {
                     @Override
                     public String[] call() throws Exception {
                         String[] res = null;
+                        String filePath = null;
                         try {
-                            res = generatePQData(pid, dataPath);
+                            List<String> snList = new ArrayList<>();
+                            filePath = "/home/lee/文档/out/PID-" + pid + "-" + new Date() + "-" + System.currentTimeMillis() + ".csv";
+                            File file = new File(filePath);
+                            if (!file.exists()) {
+                                file.createNewFile();
+                            }
+                            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+
+                            String data;
+                            String oldSN = null;
+                            PQData pqData = null;
+                            Set<String> invalidCount = new HashSet<>();
+
+                            //第一遍按顺序读取数据，记录异常数据 SN
+                            //异常原因是因为 SN 数据不连贯
+                            BufferedReader reader = new BufferedReader(new FileReader(dataPath));
+                            while ((data = reader.readLine()) != null) {
+                                String[] logs = data.split("\t");
+                                String sn ;
+                                if (logs.length > 2) {
+                                    sn = logs[1].replace("\"", "");
+                                }else {
+                                    continue;
+                                }
+                                if (oldSN == null || !oldSN.equals(sn)) {
+                                    if (pqData != null && !snList.contains(pqData.getSN())) {
+                                        if (pqData.containNull()) {
+                                            invalidCount.add(pqData.getSN());
+                                        } else {
+                                            //判断是否为需要提取的产品数据 PID
+                                            if (pqData.getSN().startsWith(pid)) {
+                                                writer.append(pqData.toCSVNew());
+                                                snList.add(pqData.getSN());
+                                            }
+                                        }
+
+                                    }
+                                    //System.out.println("new SN,we create new PQData");
+                                    pqData = new PQData();
+                                    pqData.setSN(sn);
+                                } else {
+                                    //System.out.println("old sn " + sn);
+                                }
+                                pqDataParse(pqData, data);
+                                oldSN = sn;
+                                //System.out.println(pqData.toString());
+                            }
+                            reader.close();
+
+                            //System.out.println("invalid sn list = " + Arrays.toString(invalidCount.toArray()));
+                            //System.out.println("invalid sn count = " + invalidCount.size());
+
+                            Map<String, PQData> fixedMap = new HashMap<>();
+                            for (String sn : invalidCount) {
+                                if (sn.matches("[0-9]{13}")) {
+                                    PQData temp = new PQData();
+                                    temp.setSN(sn);
+                                    fixedMap.put(sn, temp);
+                                }
+                            }
+                            //第二遍重新查询异常 SN 的数据
+                            reader = new BufferedReader(new FileReader(dataPath));
+                            while ((data = reader.readLine()) != null) {
+                                for (String sn : invalidCount) {
+                                    if (!sn.matches("[0-9]{13}")) {
+                                        continue;
+                                    }
+                                    if (data.contains(sn.substring(5))) {
+                                        //System.out.println("find fixed Data");
+                                        PQData temp = fixedMap.get(sn);
+                                        if (temp != null) {
+                                            pqDataParse(temp, data);
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+
+                            for (String sn : invalidCount) {
+                                if (sn.startsWith(pid)) {
+                                    writer.append(fixedMap.get(sn).toCSVNew());
+                                    //System.out.println("fixed sn PQ data = " + fixedMap.get(sn).toCSVNew());
+                                }
+                            }
+
+                            reader.close();
+                            writer.flush();
+                            writer.close();
                         } catch (IOException e) {
                             e.printStackTrace();
                             System.out.println("ops IO error");
-                        }finally {
+                        } finally {
                             latch.countDown();
                         }
-                        return res;
+                        return new String[]{pid, filePath};
                     }
                 });
+                Thread.sleep(20);
             }
         }
         System.out.println("total Task Size = " + taskSize);
@@ -92,7 +173,7 @@ public class PQParse {
         for (int i = 0; i < taskSize; i++) {
             Future<String[]> task = ThreadUtils.getCompletionService().take();
             String[] res = task.get();
-            System.out.println("FinalCSV task Get = "+ Arrays.toString(res));
+            System.out.println("FinalCSV task Get = " + Arrays.toString(res));
             if (mapPidPath.containsKey(res[0])) {
                 mapPidPath.get(res[0]).add(res[1]);
             } else {
@@ -130,12 +211,21 @@ public class PQParse {
                         //写入 CSV 标题
                         writer.append("SN,N_RGain,C_RGain,W_RGain,N_GGain,C_GGain,W_GGain,N_BGain,C_BGain,W_BGain,W_ROffset,W_GOffset,W_BOffset,C_ROffset,C_GOffset,C_BOffset,N_ROffset,N_GOffset,N_BOffset,TimeStamp\n");
                         for (PQData value : result.values()) {
+                            if (value.containNull()) {
+                                continue;
+                            }
                             writer.write(value.toCSVNew());
                         }
                         writer.flush();
                         writer.close();
                     }
 
+                    for (List<String> value : mapPidPath.values()) {
+                        for (String path : value) {
+                            File file = new File(path);
+                            file.delete();
+                        }
+                    }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -145,7 +235,7 @@ public class PQParse {
     }
 
 
-    private static void pqDataParse(PQData pqData, String data) {
+    private static synchronized void pqDataParse(PQData pqData, String data) {
         String value = parseData(data);
         if (data.contains("读取标准色温白平衡红色增益")) {
             pqData.setNormalGainRed(value);
@@ -221,11 +311,11 @@ public class PQParse {
         }
     }
 
-    private static String parseData(String data) {
+    private static synchronized String parseData(String data) {
         String[] logs = data.split("\t");
         String res = null;
         //String res = data.substring(69, 73);
-        if (logs.length > 6){
+        if (logs.length > 6) {
             res = logs[6];
             if (res.contains("\"")) res = res.replace("\"", "");
             //System.out.println("parse data " + res);
@@ -233,128 +323,45 @@ public class PQParse {
         return res;
     }
 
-    private static String parseTimeStamp(String data) {
-        if (data.length() < 96) {
-            return null;
+    private static synchronized String parseTimeStamp(String data) {
+        String res = null;
+        String[] params = data.split("\t");
+        if (params.length > 7) {
+            res = params[7];
+            if (res.contains("\"")) res = res.replace("\"", "");
+            //System.out.println("parse data " + res);
         }
-        String res = data.substring(76, 94);
-        if (res.contains("\"")) res = res.replace("\"", "");
-        //System.out.println("parse data " + res);
         return res;
     }
 
-    private static String[] generatePQData(String PID, String dataPath) throws IOException {
-        List<String> snList = new ArrayList<>();
-        String filePath = "/home/lee/文档/out/PID-" + PID + "-" + new Date() + "-" + System.currentTimeMillis() + ".csv";
-        File file = new File(filePath);
-        if (!file.exists()) {
-            file.createNewFile();
-        }
-        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
 
-        String data;
-        String oldSN = null;
-        PQData pqData = null;
-        Set<String> invalidCount = new HashSet<>();
-
-        //第一遍按顺序读取数据，记录异常数据 SN
-        //异常原因是因为 SN 数据不连贯
-        BufferedReader reader = new BufferedReader(new FileReader(dataPath));
-        while ((data = reader.readLine()) != null) {
-            String[] logs = data.split("\t");
-            String sn = data.substring(12, 26);
-            if (logs.length > 2) {
-                sn = logs[1].replace("\"","");
-            }
-            if (oldSN == null || !oldSN.equals(sn)) {
-                if (pqData != null && !snList.contains(pqData.getSN())) {
-                    if (pqData.containNull()) {
-                        invalidCount.add(pqData.getSN());
-                    } else {
-                        //判断是否为需要提取的产品数据 PID
-                        if (pqData.getSN().startsWith(PID)) {
-                            writer.append(pqData.toCSVNew());
-                            snList.add(pqData.getSN());
-                        }
-                    }
-
-                }
-                //System.out.println("new SN,we create new PQData");
-                pqData = new PQData();
-                pqData.setSN(sn);
-            } else {
-                //System.out.println("old sn " + sn);
-            }
-            pqDataParse(pqData, data);
-            oldSN = sn;
-            //System.out.println(pqData.toString());
-        }
-        reader.close();
-
-        System.out.println("invalid sn list = " + Arrays.toString(invalidCount.toArray()));
-        System.out.println("invalid sn count = " + invalidCount.size());
-
-        Map<String, PQData> fixedMap = new HashMap<>();
-        for (String sn : invalidCount) {
-            if (sn.matches("[0-9]{13}")) {
-                PQData temp = new PQData();
-                temp.setSN(sn);
-                fixedMap.put(sn, temp);
-            }
-        }
-        //第二遍重新查询异常 SN 的数据
-        reader = new BufferedReader(new FileReader(dataPath));
-        while ((data = reader.readLine()) != null) {
-            for (String sn : invalidCount) {
-                if (!sn.matches("[0-9]{13}")){
-                    continue;
-                }
-                if (data.contains(sn.substring(5))) {
-                    //System.out.println("find fixed Data");
-                    PQData temp = fixedMap.get(sn);
-                    if (temp != null) {
-                        pqDataParse(temp, data);
-                    }
-                    break;
-                }
-            }
-        }
-
-        for (String sn : invalidCount) {
-            if (sn.startsWith(PID)) {
-                writer.append(fixedMap.get(sn).toCSVNew());
-                System.out.println("fixed sn PQ data = " + fixedMap.get(sn).toCSVNew());
-            }
-        }
-
-        reader.close();
-        writer.flush();
-        writer.close();
-
-        return new String[]{PID, filePath};
-    }
-
-    private static void parseCSV(Map<String, PQData> result, String data) {
+    private static synchronized void parseCSV(Map<String, PQData> result, String data) {
         String[] params = data.split(",");
         if (params.length != 20) {
-            System.out.println("PQ data is illegal content=" + Arrays.toString(params));
-            System.out.println("PQ data len is illegal " + params.length);
+            //System.out.println("PQ data is illegal content=" + Arrays.toString(params));
+            //System.out.println("PQ data len is illegal " + params.length);
+            return;
+        }
+        if (data.contains("null")) {
             return;
         }
         if (result.containsKey(params[0])) {
             String oldTime = result.get(params[0]).getTimeStamp();
-            SimpleDateFormat sdf = new SimpleDateFormat();
             try {
                 Date oldD = new Date(oldTime);
                 Date newD = new Date(params[19]);
                 if (oldD.before(newD)) {
-                    result.get(params[0]).setTimeStamp(params[19]);
+                    PQData pd = new PQData(
+                            params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7],
+                            params[8], params[9], params[10], params[11], params[12], params[13], params[14], params[15],
+                            params[16], params[17], params[18], params[19]
+                    );
+                    result.put(params[0], pd);
                 }
-            }catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 e.printStackTrace();
                 result.get(params[0]).setTimeStamp(params[19]);
             }
-
 
         } else {
             PQData pd = new PQData(
